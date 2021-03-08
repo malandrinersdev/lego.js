f971a350d9c6cd457c2ee510790825cef64f3961
Sergio Edo <sergio.edo@gmail.com>
2021-03-08 23:18:09 +0100

    insignias de la comunidad - frontend

---
+++ js/usuarios.js
@@ -0,0 +1,35 @@
+
+export const getUsuarios = () => {
+
+    // A falta de base de datos...
+    const usuarios = [
+        'twochemist',
+        'terepebernal',
+        'skcode7',
+        'javideveloper',
+        'ejdiezfraile',
+        'dapaspei',
+        'Cainuriel',
+        'adriacobo',
+        'alexhermida',
+        'prinhelmet',
+        'ajmasia',
+        'ThePinkDeveloper',
+        'xurxof',
+        'sergioedo',
+        'Santi1965',
+        'migbara',
+        'Esemega',
+        'delineas'
+    ]
+
+    // A morir de exito!
+    // usuarios.push(...usuarios)
+    // usuarios.push(...usuarios)
+    // usuarios.push(...usuarios)
+    // usuarios.push(...usuarios)
+    // usuarios.push(...usuarios)
+    // usuarios.push(...usuarios)
+
+    return usuarios
+}
