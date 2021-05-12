c1a647f2912c016b06e4d6c7577183f4979d1a4a
Sergio Edo <sergio.edo@gmail.com>
2021-05-13 00:35:33 +0200

    renaming badges API and clients

--- src/insigniasAPI.js
+++ src/insigniasAPI.js
@@ -17,18 +17,17 @@ const DEFAULT_CACHE_TTL = 24 * 60 * 60 * 1000
 
 const router = express.Router()
 
-const refrescarCache = (req, res, next, ttl) => {
-    const usuarios = getUsers()
-    obtenerInsigniasUsuarios(usuarios)
-        .then((insignias) => {
+const refreshCache = (req, res, next, ttl) => {
+    const users = getUsers()
+    getUsersBadges(users)
+        .then((badges) => {
             // Cache global (todos los usuarios)
-            setValueToCache('/', insignias, ttl)
+            setValueToCache('/', badges, ttl)
 
             //Cache por usuario
-            usuarios.map((usuario) => {
-                const insigniasUsuario =
-                    insignias.filter((iu) => iu.usuario === usuario) || []
-                setValueToCache(`/${usuario}`, insigniasUsuario[0], ttl)
+            users.map((user) => {
+                const userBadges = badges.filter((ub) => ub.user === user) || []
+                setValueToCache(`/${user}`, userBadges[0], ttl)
             })
 
             res.json({ cacheKeys: getCacheKeys() })
@@ -39,18 +38,18 @@ const refrescarCache = (req, res, next, ttl) => {
 //CACHE: Webhook para actualizar la cache
 router.post('/cache/refresh/:ttl?', (req, res, next) => {
     const ttl = req.params.ttl ? Number(req.params.ttl) : undefined
-    const usuario = JSON.parse(req.body.payload).sender.login || 'anonymous'
-    if (checkUsuario(usuario)) {
-        refrescarCache(req, res, next, ttl)
+    const user = JSON.parse(req.body.payload).sender.login || 'anonymous'
+    if (checkUser(user)) {
+        refreshCache(req, res, next, ttl)
     } else {
-        next({ status: 403, message: `Usuario no autorizado` })
+        next({ status: 403, message: `Not authorized user` })
     }
 })
 
 // CACHE: Si conocemos los posibles valores, prepoblamos la cache (evitar el primer MISS)
 router.get('/cache/refresh/:ttl?', (req, res, next) => {
     const ttl = req.params.ttl ? Number(req.params.ttl) : undefined
-    refrescarCache(req, res, next, ttl)
+    refreshCache(req, res, next, ttl)
 })
 
 // CACHE: si esta en cache, ya podemos contestar
@@ -65,55 +64,55 @@ router.get('*', (req, res, next) => {
 
 // API: Insignias de todos los usuarios
 router.get('/', (req, res, next) => {
-    const usuarios = getUsers()
-    obtenerInsigniasUsuarios(usuarios)
-        .then((insignias) => {
-            res.locals.APIResponse = insignias
+    const users = getUsers()
+    getUsersBadges(users)
+        .then((badges) => {
+            res.locals.APIResponse = badges
             next()
         })
         .catch((error) => {
             next({
                 status: 500,
-                message: `No se han podido obtener los diario de los usuarios`,
+                message: `Error fetching user diaries`,
             })
         })
 })
 
 // API: Insignias de un usuario
-router.get('/:usuario*', (req, res, next) => {
-    const usuario = req.params.usuario
-    const check = checkUsuario(usuario)
+router.get('/:user*', (req, res, next) => {
+    const user = req.params.user
+    const check = checkUser(user)
     if (check.error) {
         next({ status: 400, message: check.text })
     } else {
-        obtenerInsigniasUsuarios([usuario])
-            .then((insigniasUsuario) => {
-                res.locals.APIResponse = insigniasUsuario[0]
+        getUsersBadges([user])
+            .then((userBadges) => {
+                res.locals.APIResponse = userBadges[0]
                 next()
             })
             .catch((err) => {
                 next({
                     status: 500,
-                    message: `No se ha podido obtener el diario del usuario ${usuario}`,
+                    message: `Error fetching user ${user} diary`,
                 })
             })
     }
 })
 
 // API: insignias con shields.io
-router.get('/:usuario/humor/:humor', (req, res, next) => {
-    const humor = req.params.humor
+router.get('/:user/humor/:humour', (req, res, next) => {
+    const humour = req.params.humour
     createProxyMiddleware({
         target: 'https://img.shields.io/',
         changeOrigin: true,
         pathRewrite: (path, reqProxy) => {
-            const emojiHumor = obtenerEmojiDeHumor(humor)
-            const colorHumor = obtenerColorDeHumor(humor)
-            const humorUsuario =
+            const humourEmoji = getHumourEmoji(humour)
+            const humourColor = getHumourColor(humour)
+            const userHumour =
                 (res.locals.APIResponse &&
-                    res.locals.APIResponse.humor[humor]) ||
+                    res.locals.APIResponse.humour[humour]) ||
                 0
-            return `/badge/${emojiHumor}-${humorUsuario}-${colorHumor}`
+            return `/badge/${humourEmoji}-${userHumour}-${humourColor}`
         },
         onProxyRes: (proxyRes, req, res) => {
             proxyRes.headers['Cache-control'] = 'no-store' // NO CACHE!
@@ -144,52 +143,52 @@ router.use(function (err, req, res, next) {
 // --------------------------------------
 
 // comprobacion de parámetros
-const checkUsuario = (usuario) => {
-    if (!usuario) {
-        return { error: true, text: 'Debe indicar un usuario' }
+const checkUser = (user) => {
+    if (!user) {
+        return { error: true, text: 'You must set a user' }
     } else {
-        const usuarioEncontrado = getUsers().find((u) => u === usuario)
-        if (usuarioEncontrado !== undefined) {
+        const foundUser = getUsers().find((u) => u === user)
+        if (foundUser !== undefined) {
             return { error: false }
         } else {
-            return { error: true, text: 'El usuario no és valido' }
+            return { error: true, text: 'Invalid user' }
         }
     }
 }
 
-const obtenerInsigniasUsuarios = (usuarios) => {
+const getUsersBadges = (users) => {
     const commit = 'main'
-    const asyncInsignias = usuarios.map((usuario) => {
-        return getUserDiary(usuario, commit).then((diarioMD) => {
-            const humor = getDiaryHumour(diarioMD)
-            const insignias = getDiaryBadges(diarioMD)
-            return { usuario, humor, insignias }
+    const asyncBadges = users.map((user) => {
+        return getUserDiary(user, commit).then((diarioMD) => {
+            const humour = getDiaryHumour(diarioMD)
+            const badges = getDiaryBadges(diarioMD)
+            return { user, humour, badges }
         })
     })
-    return Promise.all(asyncInsignias).then((insignias) => {
-        const insigniasOrdenadas = insignias.sort((a, b) =>
-            a.usuario.toLowerCase() > b.usuario.toLocaleLowerCase() ? 1 : -1
+    return Promise.all(asyncBadges).then((badges) => {
+        const sortedBadges = badges.sort((a, b) =>
+            a.user.toLowerCase() > b.user.toLocaleLowerCase() ? 1 : -1
         )
-        return insigniasOrdenadas
+        return sortedBadges
     })
 }
 
-const obtenerEmojiDeHumor = (humor) => {
-    const emojisHumor = {
+const getHumourEmoji = (humour) => {
+    const emojisHumour = {
         grinning: '%F0%9F%98%80',
         neutral_face: '%F0%9F%98%90',
         frowning_face: '%F0%9F%98%95',
     }
-    return emojisHumor[humor] || 'humor'
+    return emojisHumour[humour] || 'humour'
 }
 
-const obtenerColorDeHumor = (humor) => {
-    const colorHumor = {
+const getHumourColor = (humour) => {
+    const colorHumour = {
         grinning: 'green',
         neutral_face: 'blue',
         frowning_face: 'red',
     }
-    return colorHumor[humor] || 'inactive'
+    return colorHumour[humour] || 'inactive'
 }
 
 // --------------------------------------
